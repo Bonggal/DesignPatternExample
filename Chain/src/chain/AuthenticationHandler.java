50e684864a87d421b9c2591e6ce40fddeda1eef8
Bonggal <48589936+Bonggal@users.noreply.github.com>
2019-10-23 10:14:13 +0700

    Update AuthenticationHandler.java

--- Chain/src/chain/AuthenticationHandler.java
+++ Chain/src/chain/AuthenticationHandler.java
@@ -1,18 +1,24 @@
 package chain;
 
+// Activity 1: Do authentication
 public class AuthenticationHandler implements Handler {
     private Handler nextHandler;
 
+    // Set the next activity or process
     @Override
     public void nextHandler(Handler nextHandler) {
         this.nextHandler=nextHandler;
     }
 
+    // Activity process
     @Override
     public Data process(Data data) {
         System.out.println("Authentication process");
+        
+        // Doing the authentication process here
 
         if (nextHandler != null) {
+            // Calling the next activity
             data = nextHandler.process(data);
         }
         return data;
