ad8756f24f320e912c8dcd32c925b588eaa308f5
Bonggal <48589936+Bonggal@users.noreply.github.com>
2019-10-23 10:15:42 +0700

    Update TransactionHandler.java

--- Chain/src/chain/TransactionHandler.java
+++ Chain/src/chain/TransactionHandler.java
@@ -3,6 +3,7 @@ package chain;
 public class TransactionHandler implements Handler {
     private Handler nextHandler;
 
+    // Set the next activity or process
     @Override
     public void nextHandler(Handler nextHandler) {
         this.nextHandler = nextHandler;
@@ -11,9 +12,12 @@ public class TransactionHandler implements Handler {
     @Override
     public Data process(Data data) {
         System.out.println("Transaction process");
+        
+        // Doing the transaction process
         data = new Data("Header changed", "Content changed");
 
         if (nextHandler != null) {
+            // Call the next activity
             data = nextHandler.process(data);
         }
         return data;
